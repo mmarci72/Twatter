ed330ca13b73d3e055c99cf06f163a9315f245c5
mmarci72 <mmarci72@gmail.com>
2026-06-05 20:10:09 +0200

    feat: Add skeleton spring back end

---
+++ twatter-backend/src/main/java/org/twatter/twatterbackend/TwatterBackendApplication.java
@@ -0,0 +1,13 @@
+package org.twatter.twatterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwatterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwatterBackendApplication.class, args);
+    }
+
+}
---
+++ twatter-backend/src/test/java/org/twatter/twatterbackend/TwatterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.twatter.twatterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwatterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
